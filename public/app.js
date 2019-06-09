47d24c7cffde21c6d2760a62491ccb12c1223a43
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-06-10 00:27:16 +0200

    simple compononet

--- public/app.js
+++ public/app.js
@@ -1,6 +1,16 @@
 "use strict"
 
+// compononet must start with capitalletter and have one method at lest render method
+// or es 2015 class ReactDom extends React.Component{
+var ReactDemo = React.createClass({
+    render: function(){
+        return(
+            <div>This is a simple Reactjs component</div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <ReactDemo />,
     document.getElementById('content')
 );
