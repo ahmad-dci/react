7d003b2d122461bb3aa0f4303f4c950090f0cebd
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-06-10 14:15:25 +0200

    multi compononents

--- public/app.js
+++ public/app.js
@@ -5,7 +5,28 @@
 var ReactDemo = React.createClass({
     render: function(){
         return(
+            <div>
+            <CommentBox />
+            <EventGenerator />
             <div>This is a simple Reactjs component</div>
+            </div>
+        );
+    }
+});
+
+var CommentBox = React.createClass({
+    render: function(){
+        return(
+            <div>This will be a comment box</div>
+        );
+    }
+});
+var EventGenerator = React.createClass({
+    render: function(){
+        return(
+            <button>
+                Click me
+            </button>
         );
     }
 });
